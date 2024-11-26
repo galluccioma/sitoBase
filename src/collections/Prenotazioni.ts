fe0c37df35e59f88b1fa2795e47a87360b0f2f83
galluccioma <galluccioma@gmail.com>
2024-11-26 17:38:16 +0100

    pup

--- src/collections/Prenotazioni.ts
+++ src/collections/Prenotazioni.ts
@@ -1,6 +1,7 @@
 import { CollectionConfig } from 'payload'
 // import { sendClientConfirmationWithQRCode } from '../mail/emailService'
 
+
 export const Prenotazioni: CollectionConfig = {
   slug: 'prenotazioni',
   access: {
@@ -88,75 +89,6 @@ export const Prenotazioni: CollectionConfig = {
     },
   ],
   hooks: {
-    // afterChange: [
-    //   async ({ operation, doc, req }) => {
-    //     try {
-    //       if (operation === 'create' && doc.stato === 'nuovo') {
-    //         for (const item of doc.carrello) {
-    //           const itemId = item.biglietto;
-    //           const fasciaOrariaSelezionata = item.fasciaOrariaSelezionata;
-    //           const requestedQuantity = item.quantità;
-    
-    //           // Trova il tipoBiglietto
-    //           const tipoBiglietto = await req.payload.findByID({
-    //             collection: 'biglietti',
-    //             id: itemId,
-    //           });
-    
-    //           if (!tipoBiglietto) {
-    //             throw new Error(`Biglietto con ID ${itemId} non trovato.`);
-    //           }
-    
-    //           const tipoBigliettoSelezionato = tipoBiglietto.tipoBiglietto;
-    
-    //           // Normalizza la data
-    //           const normalizedDate = new Date(doc.dataPrenotazione);
-    //           normalizedDate.setUTCHours(0, 0, 0, 0);
-    //           const isoDateOnly = normalizedDate.toISOString().split('T')[0];
-    
-    //           // Verifica la disponibilità
-    //           let existingDisponibilita = await req.payload.find({
-    //             collection: 'disponibilita',
-    //             where: {
-    //               tipoBiglietto: { equals: tipoBigliettoSelezionato },
-    //               fasciaOraria: { equals: fasciaOrariaSelezionata },
-    //               data: { equals: `${isoDateOnly}T00:00:00.000Z` },
-    //             },
-    //           });
-    
-    //           if (existingDisponibilita.docs.length === 0) {
-    //             // Se non esistono disponibilità, creiamo una nuova voce
-    //             const postiDisponibili = tipoBigliettoSelezionato === 'visita_guidata' ? 25 : 18;
-    //             await req.payload.create({
-    //               collection: 'disponibilita',
-    //               data: {
-    //                 tipoBiglietto: tipoBigliettoSelezionato,
-    //                 fasciaOraria: fasciaOrariaSelezionata,
-    //                 data: normalizedDate.toISOString(),
-    //                 disponibilità: postiDisponibili - requestedQuantity,
-    //               },
-    //             });
-    //           } else {
-    //             // Se esiste già disponibilità, aggiorniamo la quantità
-    //             const availableSlot = existingDisponibilita.docs[0];
-    //             const newDisponibilita = availableSlot.disponibilità - requestedQuantity;
-    //             if (newDisponibilita < 0) {
-    //               throw new Error(`Non ci sono abbastanza posti disponibili per la fascia oraria ${fasciaOrariaSelezionata} il giorno ${isoDateOnly}.`);
-    //             }
-    //             await req.payload.update({
-    //               collection: 'disponibilita',
-    //               id: availableSlot.id,
-    //               data: { disponibilità: newDisponibilita },
-    //             });
-    //           }
-    //         }
-    //       }
-    //     } catch (error) {
-    //       console.error(`Errore durante il processo di afterChange: ${error.message}`);
-    //       throw error;
-    //     }
-    //   },
-    // ],
     
   }
   
