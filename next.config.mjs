6d15eeb49be512140f38b47f36dea5357fbaf162
galluccioma <galluccioma@gmail.com>
2024-12-05 11:20:46 +0100

    PROVA CON FRONTEND RIMOSSO

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/admin',
+          permanent: true,
+        },
+      ]
+    },
+}
+
+export default withPayload(nextConfig)
+
+
